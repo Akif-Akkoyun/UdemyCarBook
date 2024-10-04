3be792fd9322b2f1447b40c35697b6a2721ec1b5
Akif Akkoyun <akifakkoyun09@gmail.com>
2024-10-05 01:06:16 +0300

    add mvc and add theme

---
+++ Fronteds/UdemyCarBook.WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartials.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UdemyCarBook.Dto.AboutDtos;
+
+namespace UdemyCarBook.WebUI.ViewComponents.AboutViewComponents
+{
+    public class _AboutUsComponentPartials : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AboutUsComponentPartials(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responsMessage = await client.GetAsync("https://localhost:7219/api/Abouts");
+
+            if (responsMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responsMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
