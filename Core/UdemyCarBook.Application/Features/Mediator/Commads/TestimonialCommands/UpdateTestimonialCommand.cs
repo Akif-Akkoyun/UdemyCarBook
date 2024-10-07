59f2cf32a0b37858352b25f76355aac1474e0016
Akif Akkoyun <akifakkoyun09@gmail.com>
2024-10-07 18:39:19 +0300

    Add viewcomponenet
    Add viewcomponenet services,about.
    add dtos

---
+++ Core/UdemyCarBook.Application/Features/Mediator/Commads/TestimonialCommands/UpdateTestimonialCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.Application.Features.Mediator.Commads.TestimonialCommands
+{
+    public class UpdateTestimonialCommand : IRequest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Title { get; set; } = default!;
+        public string Comment { get; set; } = default!;
+        public string ImageUrl { get; set; } = default!;
+    }
+}
---
+++ Core/UdemyCarBook.Application/Features/Mediator/Handlers/TestimonialHandlers/UpdateTestimonialCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.Mediator.Commads.TestimonialCommands;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.Mediator.Handlers.TestimonialHandlers
+{
+    public class UpdateTestimonialCommandHandler : IRequestHandler<UpdateTestimonialCommand>
+    {
+        private readonly IRepository<TestimonialEntity> _repository;
+
+        public UpdateTestimonialCommandHandler(IRepository<TestimonialEntity> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateTestimonialCommand request, CancellationToken cancellationToken)
+        {
+            var value = await _repository.GetByIdAsync(request.Id);
+            value.Name = request.Name;
+            value.Comment = request.Comment;
+            value.Title = request.Title;
+            value.ImageUrl = request.ImageUrl;
+            await _repository.UpdateAsync(value);
+        }
+    }
+}
