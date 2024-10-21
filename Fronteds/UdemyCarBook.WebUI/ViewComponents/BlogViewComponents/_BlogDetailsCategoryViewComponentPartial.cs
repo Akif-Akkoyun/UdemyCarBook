22194c41e52f6cc50e6c563c790446826c54f18c
Akif Akkoyun <akifakkoyun09@gmail.com>
2024-10-21 14:19:00 +0300

    blog categories consume

--- Fronteds/UdemyCarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsCategoryViewComponentPartial.cs
+++ Fronteds/UdemyCarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsCategoryViewComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UdemyCarBook.Dto.CategoryDtos;
 
 namespace UdemyCarBook.WebUI.ViewComponents.BlogViewComponents
 {
     public class _BlogDetailsCategoryViewComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogDetailsCategoryViewComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7219/api/Categories");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var testimonials = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(testimonials);
+            }
             return View();
         }
     }
