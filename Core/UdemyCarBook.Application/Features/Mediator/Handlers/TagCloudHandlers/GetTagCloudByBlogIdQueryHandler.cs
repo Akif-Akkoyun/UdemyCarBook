cadd85f94bf0fdb02a2f39b9b9c79560cfe6724c
Akif Akkoyun <akifakkoyun09@gmail.com>
2024-10-22 21:27:27 +0300

    add blog details area
    I enabled the blog details to be displayed by ID. Thus, separate contents can be displayed for the details of all added blogs. And we wrote an entity-specific repository.

---
+++ Core/UdemyCarBook.Application/Features/Mediator/Handlers/TagCloudHandlers/GetTagCloudByBlogIdQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.Mediator.Queries.TagCloudQueries;
+using UdemyCarBook.Application.Features.Mediator.Results.TagCloudResults;
+using UdemyCarBook.Application.Interfaces.TagCloudInterfaces;
+
+namespace UdemyCarBook.Application.Features.Mediator.Handlers.TagCloudHandlers
+{
+    public class GetTagCloudByBlogIdQueryHandler : IRequestHandler<GetTagCloudByBlogIdQuery, List<GetTagCloudByBlogIDQueryResult>>
+    {
+        private readonly ITagCloudRepository _repository;
+
+        public GetTagCloudByBlogIdQueryHandler(ITagCloudRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<GetTagCloudByBlogIDQueryResult>> Handle(GetTagCloudByBlogIdQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Fronteds/UdemyCarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsTagCloudViewComponentPartial.cs
+++ Fronteds/UdemyCarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsTagCloudViewComponentPartial.cs
@@ -1,11 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UdemyCarBook.Dto.BlogDtos;
 
 namespace UdemyCarBook.WebUI.ViewComponents.BlogViewComponents
 {
     public class _BlogDetailsTagCloudViewComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _BlogDetailsTagCloudViewComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync($"https://localhost:7219/api/Blogs/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var testimonials = JsonConvert.DeserializeObject<GetBlogByIdDto>(jsonData);
+                return View(testimonials);
+            }
             return View();
         }
     }
