d0b573c42b92c3111726ed8a83826c9d03030967
Akif Akkoyun <akifakkoyun09@gmail.com>
2024-10-22 17:45:57 +0300

    add TagCloud Entity and Source

---
+++ Core/UdemyCarBook.Application/Features/Mediator/Handlers/TagCloudHandlers/UpdateTagCloudCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.Mediator.Commads.TagCloudCommands;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.Mediator.Handlers.TagCloudHandlers
+{
+    public class UpdateTagCloudCommandHandler : IRequestHandler<UpdateTagCloudCommand>
+    {
+        private readonly IRepository<TagCloudEntity> _repository;
+
+        public UpdateTagCloudCommandHandler(IRepository<TagCloudEntity> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateTagCloudCommand request, CancellationToken cancellationToken)
+        {
+            var value = await _repository.GetByIdAsync(request.Id);
+            value.Title = request.Title;
+            value.BlogID = request.BlogID;
+            await _repository.UpdateAsync(value);
+        }
+    }
+}
