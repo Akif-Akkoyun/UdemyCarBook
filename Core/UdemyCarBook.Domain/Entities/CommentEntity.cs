fc9c5611e94a3b0575fc45addb011d05c98d4618
Akif Akkoyun <akifakkoyun09@gmail.com>
2025-01-15 13:58:18 +0300

    comment
    An email item has been added to the comment table in data based. Adding a comment is still working.

--- Core/UdemyCarBook.Domain/Entities/CommentEntity.cs
+++ Core/UdemyCarBook.Domain/Entities/CommentEntity.cs
@@ -13,6 +13,7 @@ namespace UdemyCarBook.Domain.Entities
         public string Name { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
         public string Description { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
         public int BlogId { get; set; }
         public BlogEntity Blog { get; set; }
     }
