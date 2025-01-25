dd01ffa503d902d12736bfb73c06d09601861dfa
Akif Akkoyun <akifakkoyun09@gmail.com>
2025-01-25 23:00:35 +0300

    Login and Register Page
    1-Add login andregister page with controller.
    2-LoginApi and RegisterApi tested.
    3-There will be no password renewal.

---
+++ Core/UdemyCarBook.Application/Features/Mediator/Handlers/AppUserHandlers/CreateAppUserCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Enums;
+using UdemyCarBook.Application.Features.Mediator.Commads.RegisterCommands;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.Mediator.Handlers.AppUserHandlers
+{
+    public class CreateAppUserCommandHandler : IRequestHandler<CreateAppUserCommand>
+    {
+        private readonly IRepository<AppUserEntity> _repository;
+
+        public CreateAppUserCommandHandler(IRepository<AppUserEntity> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateAppUserCommand request, CancellationToken cancellationToken)
+        {
+            await _repository.CreatAsync(new AppUserEntity
+            {
+                UserName = request.UserName,
+                Password = request.Password,
+                AppRoleId = (int)Rolestype.Member,
+                Mail = request.Mail,
+                Name = request.Name,
+                Surname = request.Surname
+            });
+
+        }
+    }
+}
---
+++ Frontends/UdemyCarBook.Dto/RegisterDtos/CreateRegisterDto.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace UdemyCarBook.Dto.RegisterDtos
+{
+    public class CreateRegisterDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Mail { get; set; }
+    }
+}
