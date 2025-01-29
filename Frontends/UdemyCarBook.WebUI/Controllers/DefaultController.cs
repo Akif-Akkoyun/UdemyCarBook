c40dbb278928156aa9636b77e0a0cbc448d9d023
Akif Akkoyun <akifakkoyun09@gmail.com>
2025-01-29 16:59:35 +0300

    add-jwt-bearer-auth-token

--- Frontends/UdemyCarBook.WebUI/Controllers/DefaultController.cs
+++ Frontends/UdemyCarBook.WebUI/Controllers/DefaultController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using System.Net.Http.Headers;
 using UdemyCarBook.Dto.LocationDtos;
 
 namespace UdemyCarBook.WebUI.Controllers
@@ -16,18 +17,22 @@ namespace UdemyCarBook.WebUI.Controllers
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7219/api/Location");
-
-            var jsonData = await responseMessage.Content.ReadAsStringAsync();
-            var values = JsonConvert.DeserializeObject<List<ResultLocationDto>>(jsonData);               
-            List<SelectListItem>values2 =(from x in values
-                                          select new SelectListItem
-                                          {
-                                              Text = x.Name,
-                                              Value = x.Id.ToString()
-                                          }).ToList();
-            ViewBag.v=values2;
+            var token = User.Claims.FirstOrDefault(c => c.Type == "accessToken").Value;
+            if (token != null)
+            {
+                var client = _httpClientFactory.CreateClient();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                var responseMessage = await client.GetAsync("https://localhost:7219/api/Location");
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultLocationDto>>(jsonData);
+                List<SelectListItem> values2 = (from x in values
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.Name,
+                                                    Value = x.Id.ToString()
+                                                }).ToList();
+                ViewBag.v = values2;
+            }
             return View();
         }
         [HttpPost]
--- Frontends/UdemyCarBook.WebUI/Program.cs
+++ Frontends/UdemyCarBook.WebUI/Program.cs
@@ -1,15 +1,25 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddCookie(JwtBearerDefaults.AuthenticationScheme, opt =>
+{
+    opt.LoginPath = "/Login/Index";
+    opt.LogoutPath = "/Login/Logout";
+    opt.AccessDeniedPath = "/Pages/AccessDenied";
+    opt.Cookie.SameSite = SameSiteMode.Strict;
+    opt.Cookie.HttpOnly = true;
+    opt.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+    opt.Cookie.Name = "CarBookJwt";
+});
 builder.Services.AddHttpClient();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -17,7 +27,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
