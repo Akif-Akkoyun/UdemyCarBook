e9a07cee230ffe28a63db169e882c4bfeebcbe90
Akif Akkoyun <akifakkoyun09@gmail.com>
2025-01-29 17:53:13 +0300

    Service Extensions
    Apart from the extensions method written for MediatR, dependency injection of repositories has been added to the ServiceExtensions method, which I added directly to web.api in the presentation layer.

---
+++ Presentation/UdemyCarBook.WebApi/ServiceExtensions.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.CQRS.Handlers.AboutHandlers;
+using UdemyCarBook.Application.Features.CQRS.Handlers.BannerHandlers;
+using UdemyCarBook.Application.Features.CQRS.Handlers.BrandHandlers;
+using UdemyCarBook.Application.Features.CQRS.Handlers.CarHandlers;
+using UdemyCarBook.Application.Features.CQRS.Handlers.CategoryHandlers;
+using UdemyCarBook.Application.Features.CQRS.Handlers.ContactHandlers;
+using UdemyCarBook.Application.Features.RepositoryPattern;
+using UdemyCarBook.Application.Interfaces.BlogInterfaces;
+using UdemyCarBook.Application.Interfaces.CarDescriptionInterfaces;
+using UdemyCarBook.Application.Interfaces.CarFeatureInterfaces;
+using UdemyCarBook.Application.Interfaces.CarInterfaces;
+using UdemyCarBook.Application.Interfaces.CarPricingInterface;
+using UdemyCarBook.Application.Interfaces.RentACarInterfaces;
+using UdemyCarBook.Application.Interfaces.ReviewInterfaces;
+using UdemyCarBook.Application.Interfaces.StatistcsInterfaces;
+using UdemyCarBook.Application.Interfaces.TagCloudInterfaces;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Persistence.Repositories.BlogRepositories;
+using UdemyCarBook.Persistence.Repositories.CarDescriptionRepositories;
+using UdemyCarBook.Persistence.Repositories.CarFeatureRepositories;
+using UdemyCarBook.Persistence.Repositories.CarPricingRepositories;
+using UdemyCarBook.Persistence.Repositories.CarRepositories;
+using UdemyCarBook.Persistence.Repositories.CommentRepositories;
+using UdemyCarBook.Persistence.Repositories.RentACarRepositories;
+using UdemyCarBook.Persistence.Repositories.ReviewRepositories;
+using UdemyCarBook.Persistence.Repositories.StatisticsRepositories;
+using UdemyCarBook.Persistence.Repositories.TagCloudRepositories;
+using UdemyCarBook.Persistence.Repositories;
+using FluentValidation.AspNetCore;
+using System.Reflection;
+
+namespace UdemyCarBook.Persistence.Context
+{
+    public static class ServiceExtensions
+    {
+        public static void ApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<CarBookContext>();
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddScoped(typeof(ICarRepository), typeof(CarRepository));
+            services.AddScoped(typeof(IBlogRepository), typeof(BlogRepository));
+            services.AddScoped(typeof(ICarPricingRepository), typeof(CarPricingRepository));
+            services.AddScoped(typeof(ITagCloudRepository), typeof(TagCloudRepository));
+            services.AddScoped(typeof(IGenericRepository<>), typeof(CommentRepository<>));
+            services.AddScoped(typeof(IStatisticsRepository), typeof(StatisticsRepository));
+            services.AddScoped(typeof(IRentACarRepository), typeof(RentACarRepository));
+            services.AddScoped(typeof(ICarFeatureRepository), typeof(CarFeatureRepository));
+            services.AddScoped(typeof(ICarDescriptionRepository), typeof(CarDescriptionRepository));
+            services.AddScoped(typeof(IReviewRepository), typeof(ReviewRepository));
+            services.AddScoped<GetAboutQueryHandler>();
+            services.AddScoped<GetAboutByIdQueryHandler>();
+            services.AddScoped<CreateAboutCommandHandler>();
+            services.AddScoped<UpdateAboutCommandHandler>();
+            services.AddScoped<RemoveAboutCommandHandler>();
+            services.AddScoped<GetBannerQueryHandler>();
+            services.AddScoped<GetBannerByIdQueryHandler>();
+            services.AddScoped<CreateBannerCommandHandler>();
+            services.AddScoped<UpdateBannerCommandHandler>();
+            services.AddScoped<RemoveBannerCommandHandler>();
+            services.AddScoped<GetBrandQueryHandler>();
+            services.AddScoped<GetBrandByIdQueryHandler>();
+            services.AddScoped<CreateBrandCommandHandler>();
+            services.AddScoped<UpdateBrandCommandHandler>();
+            services.AddScoped<RemoveBrandCommandHandler>();
+            services.AddScoped<GetCarQueryHandler>();
+            services.AddScoped<GetCarByIdQueryHandler>();
+            services.AddScoped<CreateCarCommandHandler>();
+            services.AddScoped<UpdateCarCommandHandler>();
+            services.AddScoped<RemoveCarCommandHandler>();
+            services.AddScoped<GetCarWithBrandQueryHandler>();
+            services.AddScoped<GetLast5CarsWithBrandQueryHandler>();
+            services.AddScoped<GetCategoryByIdQueryHandler>();
+            services.AddScoped<GetCategoryQueryHandler>();
+            services.AddScoped<CreateCategoryCommandHandler>();
+            services.AddScoped<UpdateCategoryCommandHandler>();
+            services.AddScoped<RemoveCategoryCommandHandler>();
+            services.AddScoped<GetContactQueryHandler>();
+            services.AddScoped<GetContactByIdQueryHandler>();
+            services.AddScoped<CreateContactCommandHandler>();
+            services.AddScoped<UpdateContactCommandHandler>();
+            services.AddScoped<RemoveContactCommandHandler>();
+
+            services.AddControllers().AddFluentValidation(x =>
+            {
+                x.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            });
+        }
+    }
+}
